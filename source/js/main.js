56d7a6c205ef179e2af7a6d1027bf4f9909236b2
phillychi3 <phillychi3@gmail.com>
2023-12-28 03:03:24 +0800

    sync

--- source/js/main.js
+++ source/js/main.js
@@ -8,3 +8,42 @@ const switch_theme = () =>{
 }
 
 document.getElementById('dark_mode_switch').addEventListener('click', switch_theme);
+
+const navbar = document.querySelector('.header');
+if (navbar) {
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 100) {
+            navbar.classList.add('header-black');
+        } else {
+            navbar.classList.remove('header-black');
+        }
+    });
+}
+
+
+const figureHighlight = document.querySelectorAll('figure.highlight');
+figureHighlight.forEach((block) => {
+    const code_top = document.createElement('div');
+    const lang = block.getAttribute('class').split(' ')[1]
+
+    code_top.innerHTML = `
+        <div class="code_top">
+            <div class="code_langname">${lang}</div>
+            <div class="copybutton">
+                <i class="fa fa-copy"></i>
+            </div>
+        </div>
+            `;
+    block.insertBefore(code_top, block.firstChild)
+});
+
+
+const tbody = document.querySelectorAll('table');
+tbody.forEach((block) => {
+    const codectx = document.createElement('div');
+    codectx.classList.add('code_table');
+    // 新增codectx div把table包起來
+    block.parentNode.insertBefore(codectx, block);
+    codectx.appendChild(block);
+
+});
